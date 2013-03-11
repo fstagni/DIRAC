134f7189f6cb25ed4f2a324f52e103f91ac3afde
FedericoStagni <fstagni@cern.ch>
2013-03-11 18:12:09 +0100

    Script inherits from ModuleBase

--- Workflow/Modules/Script.py
+++ Workflow/Modules/Script.py
@@ -1,27 +1,24 @@
-########################################################################
-# $Id$
-# File :   Script.py
-# Author : Stuart Paterson
-########################################################################
-
-""" The Script class provides a simple way for users to specify an executable
+''' The Script class provides a simple way for users to specify an executable
     or file to run (and is also a simple example of a workflow module).
-"""
-
-__RCSID__ = "$Id$"
-
-
-from DIRAC.Core.Utilities.Subprocess import shellCall
-from DIRAC                           import gLogger, S_OK, S_ERROR
+'''
 
 import os, sys, re
 
-class Script( object ):
+from DIRAC.Core.Utilities.Subprocess    import shellCall
+from DIRAC                              import gLogger, S_OK, S_ERROR
+
+from DIRAC.Workflow.Modules.ModuleBase  import ModuleBase
+
+class Script( ModuleBase ):
 
   #############################################################################
-  def __init__( self ):
-    self.log = gLogger.getSubLogger( "Script" )
-    #Set defaults for all workflow parameters here
+  def __init__( self, rm = None ):
+    ''' c'tor
+    '''
+    self.log = gLogger.getSubLogger( 'Script' )
+    super( Script, self ).__init__( self.log, rm = rm )
+
+    # Set defaults for all workflow parameters here
     self.name = ''
     self.executable = ''
     self.logFile = ''
@@ -29,10 +26,13 @@ class Script( object ):
     self.step_commons = {}
 
   #############################################################################
+
   def resolveInputVariables( self ):
-    """ By convention the workflow parameters are resolved here.
-    """
-    result = S_OK()
+    ''' By convention the workflow parameters are resolved here.
+    '''
+    super( Script, self )._resolveInputVariables()
+    super( Script, self )._resolveInputStep()
+
     if self.step_commons.has_key( 'name' ):
       self.name = self.step_commons['name']
     else:
@@ -54,59 +54,74 @@ class Script( object ):
     if self.step_commons.has_key( 'arguments' ):
       self.arguments = self.step_commons['arguments']
 
-    return result
-
   #############################################################################
-  def execute( self ):
-    """ Main execution function.
-    """
-    failed = False
-    result = self.resolveInputVariables()
-    if not result['OK']:
-      return result
-    self.log.info( 'Script Module Instance Name: %s' % ( self.name ) )
-    cmd = self.executable
-    if os.path.exists( os.path.basename( self.executable ) ):
-      self.executable = os.path.basename( self.executable )
-      if not os.access( '%s/%s' % ( os.getcwd(), self.executable ), 5 ):
-        os.chmod( '%s/%s' % ( os.getcwd(), self.executable ), 0755 )
-      cmd = '%s/%s' % ( os.getcwd(), self.executable )
-    if re.search( '.py$', self.executable ):
-      cmd = '%s %s' % ( sys.executable, self.executable )
-    if self.arguments:
-      cmd = '%s %s' % ( cmd, self.arguments )
 
-    self.log.info( 'Command is: %s' % cmd )
-    outputDict = shellCall( 0, cmd )
-    if not outputDict['OK']:
-      failed = True
-      self.log.error( 'Shell call execution failed:' )
-      self.log.error( outputDict['Message'] )
-    resTuple = outputDict['Value']
-    status = resTuple[0]
-    stdout = resTuple[1]
-    stderr = resTuple[2]
-    if status:
-      failed = True
-      self.log.error( 'Non-zero status %s while executing %s' % ( status, cmd ) )
-    else:
-      self.log.info( '%s execution completed with status %s' % ( self.executable, status ) )
+  def execute( self, production_id = None, prod_job_id = None, wms_job_id = None,
+               workflowStatus = None, stepStatus = None,
+               wf_commons = None, step_commons = None,
+               step_number = None, step_id = None ):
+    ''' Main execution method.
+    '''
 
-    self.log.verbose( stdout )
-    self.log.verbose( stderr )
-    if os.path.exists( self.logFile ):
-      self.log.verbose( 'Removing existing %s' % self.logFile )
-      os.remove( self.logFile )
-    fopen = open( '%s/%s' % ( os.getcwd(), self.logFile ), 'w' )
-    fopen.write( '<<<<<<<<<< %s Standard Output >>>>>>>>>>\n\n%s ' % ( self.executable, stdout ) )
-    if stderr:
-      fopen.write( '<<<<<<<<<< %s Standard Error >>>>>>>>>>\n\n%s ' % ( self.executable, stderr ) )
-    fopen.close()
-    self.log.info( 'Output written to %s, execution complete.' % ( self.logFile ) )
+    try:
+      super( Script, self ).execute( production_id, prod_job_id, wms_job_id,
+                                     workflowStatus, stepStatus,
+                                     wf_commons, step_commons,
+                                     step_number, step_id )
 
-    if failed:
-      return S_ERROR( 'Exit Status %s' % ( status ) )
+      self._resolveInputVariables()
 
-    return S_OK()
+      self.log.info( 'Script Module Instance Name: %s' % ( self.name ) )
+      cmd = self.executable
+      if os.path.exists( os.path.basename( self.executable ) ):
+        self.executable = os.path.basename( self.executable )
+        if not os.access( '%s/%s' % ( os.getcwd(), self.executable ), 5 ):
+          os.chmod( '%s/%s' % ( os.getcwd(), self.executable ), 0755 )
+        cmd = '%s/%s' % ( os.getcwd(), self.executable )
+      if re.search( '.py$', self.executable ):
+        cmd = '%s %s' % ( sys.executable, self.executable )
+      if self.arguments:
+        cmd = '%s %s' % ( cmd, self.arguments )
 
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
+      self.log.info( 'Command is: %s' % cmd )
+      outputDict = shellCall( 0, cmd )
+      if not outputDict['OK']:
+        failed = True
+        self.log.error( 'Shell call execution failed:' )
+        self.log.error( outputDict['Message'] )
+      resTuple = outputDict['Value']
+      status = resTuple[0]
+      stdout = resTuple[1]
+      stderr = resTuple[2]
+      if status:
+        failed = True
+        self.log.error( 'Non-zero status %s while executing %s' % ( status, cmd ) )
+      else:
+        self.log.info( '%s execution completed with status %s' % ( self.executable, status ) )
+
+      self.log.verbose( stdout )
+      self.log.verbose( stderr )
+      if os.path.exists( self.logFile ):
+        self.log.verbose( 'Removing existing %s' % self.logFile )
+        os.remove( self.logFile )
+      fopen = open( '%s/%s' % ( os.getcwd(), self.logFile ), 'w' )
+      fopen.write( '<<<<<<<<<< %s Standard Output >>>>>>>>>>\n\n%s ' % ( self.executable, stdout ) )
+      if stderr:
+        fopen.write( '<<<<<<<<<< %s Standard Error >>>>>>>>>>\n\n%s ' % ( self.executable, stderr ) )
+      fopen.close()
+      self.log.info( 'Output written to %s, execution complete.' % ( self.logFile ) )
+
+      if failed:
+        return S_ERROR( 'Exit Status %s' % ( status ) )
+
+      return S_OK()
+
+    except Exception, e:
+      self.log.exception( e )
+      return S_ERROR( e )
+
+    finally:
+      super( Script, self ).finalize()
+
+
+# EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
